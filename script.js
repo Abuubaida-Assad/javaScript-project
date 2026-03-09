c66e66896e9e8ac473077a84c547197069cedc09
Abuubaida-Assad <assaduddin056464@gmail.com>
2026-03-09 16:42:10 +0530

    working

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ const blocks = []
 //     block.classList.add("block");
 //     board.appendChild(block);
 // }
-const snake = [{ x:1, y:3 },{ x:1,y:4 },{ x:1,y5 }];
+// const snake = [{ x:1, y:3 },{ x:1,y:4 },{ x:1,y5 }];
+// const snake = [
+// { x:1, y:3 }
+
+// ];
+
+const snake = [
+    {
+        x: 1, y:3
+    }
+]
 
 let direction = 'right'
 
@@ -29,7 +39,40 @@ function render() {
         blocks[`${segment.x}-${segment.y}`].classList.add("fill")
     })
 }
+
+setInterval(() => {
+    let head = null 
+
+    if (direction === "left") {
+        head = { x: snake[ 0 ].x, y: snake[ 0 ].y - 1 }
+    } else if (direction === "right"){
+        head = { x: snake[ 0 ].x, y: snake[ 0 ].y + 1 }
+    } else if (direction === "down") {
+         head = { x: snake[ 0 ].x+ 1, y: snake[ 0 ].y }
+    } else if (direction === "up") {
+        head = { x: snake[ 0 ].x- 1, y: snake[ 0 ].y }
+    }
+
+    snake.forEach(segment =>{
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+    })
+    snake.unshift(head)
+    snake.pop()
+
+
+    render()
+},300);
  
 
-
+addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp"){
+        direction="up"
+    } else if (event.key === "ArrowRight"){
+        direction = "right"
+    }else if (event.key === "ArrowLeft"){
+        direction = "left"
+    }else if (event.key === "ArrowDown"){
+        direction = "down"
+    }
+})
 

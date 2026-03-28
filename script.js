0e9d36409a43ecd5edd4becc8a52dda73090eebc
Abuubaida-Assad <assaduddin056464@gmail.com>
2026-03-28 12:24:46 +0530

    working

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const board = document.querySelector('.board');
+const startButton = document.querySelector(".btn-start")
+const modal = document.querySelector(".modal")
+const startGameModal = document.querySelector(".start-game")
+const gameOverModal = document.querySelector(".game-over")
+const restartButton = document.querySelector(".btn-restart")
 const blockHeigth = 50;
 const blockWidth = 50;
 const cols = Math.floor(board.clientWidth/blockWidth);
@@ -18,7 +23,7 @@ const blocks = []
 let intervalId = null;
 let food = {x:Math.floor(Math.random()*rows), y:Math.floor(Math.random()*cols)}
 
-const snake = [
+let snake = [
     {
         x: 1, y:3
     }
@@ -53,8 +58,15 @@ function render() {
     }
 
     if (head.x<0 || head.x>= rows || head.y<0 || head.y >= cols){
-        alert("Game Over")
+        // alert("Game Over")
         clearInterval(intervalId)
+
+        modal.style.display = "flex"
+        startGameModal.style.display = "none"
+        gameOverModal.style.display = "flex"
+
+
+        return;
     }
 
     if (head.x == food.x && head.y == food.y){
@@ -84,11 +96,35 @@ function render() {
     })
 }
 
-intervalId = setInterval(() => {
+// intervalId = setInterval(() => {
+
+//     render()
+// },300);
+ 
+startButton.addEventListener("click", () => {
+
+    modal.style.display = "none"
+    intervalId = setInterval(() => {
 
     render()
 },300);
- 
+})
+
+restartButton.addEventListener("click", restartGame)
+function restartGame() {
+    blocks[`${food.x}-${food.y}`].classList.remove("food")
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+    })
+
+    modal.style.display = "none"
+    direction = "down"
+    snake [{x:1, y:3}]
+    food = {x: Math.floor(Math.random()*rows), y:Math.floor(Math.random()*cols)}
+        intervalId=setInterval(() => {render()},300)
+    
+}
+
 
 addEventListener("keydown", (event) => {
     if (event.key === "ArrowUp"){
